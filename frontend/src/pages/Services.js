43abba42adece86ac399438f0d3ffea5a59fa862
PRIYANSHU SONI <122470603+priyanshu08soni@users.noreply.github.com>
2025-08-28 16:03:06 +0530

    commit

--- frontend/src/pages/Services.js
+++ frontend/src/pages/Services.js
@@ -29,7 +29,7 @@ const Services = () => {
   const scrollToExploreOpacity = useTransform(scrollY, [0, 150], [1, 0]);
 
   return (
-    <div className="relative h-[230vh]">
+    <div className="relative h-[250vh]">
       <Navbar />
 
       <section className="relative h-screen w-full overflow-hidden">
@@ -94,16 +94,42 @@ const Services = () => {
           transition={{ duration: 1, delay: 0.5 }}
           className="max-w-4xl"
         >
-          <h1 className="text-7xl md:text-8xl font-bold drop-shadow-2xl mb-6 text-balance">
+          <h1
+            className="text-7xl md:text-8xl drop-shadow-2xl mb-6 text-balance 
+               bg-gradient-to-r from-white via-orange-400 to-purple-600 
+               bg-clip-text text-transparent"
+          >
             Our Services
           </h1>
-          <p className="text-xl md:text-2xl opacity-90 font-light leading-relaxed text-pretty">
+          <p className="text-xl md:text-2xl text-white">
             We craft digital experiences that inspire, engage, and deliver
             exceptional results for your brand
           </p>
         </motion.div>
 
         <ServicesCard />
+
+        <motion.div
+          initial={{ opacity: 0, y: 30 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1, delay: 0.5 }}
+        >
+          <h1
+            className="text-6xl md:text-7xl drop-shadow-2xl mb-6 mt-40 text-balance
+               bg-gradient-to-r from-white via-orange-400 to-purple-600 
+               bg-clip-text text-transparent"
+          >
+            Where Creativity Meets Strategy
+          </h1>
+
+          <h1
+            className="text-6xl md:text-7xl drop-shadow-2xl mt-6 mb-16 text-balance
+               bg-gradient-to-r from-white via-orange-400 to-purple-600 
+               bg-clip-text text-transparent"
+          >
+            Design . Inspire . Deliver
+          </h1>
+        </motion.div>
         <FooterOther />
       </div>
     </div>
